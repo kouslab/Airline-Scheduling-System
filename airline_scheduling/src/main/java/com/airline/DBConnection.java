a4820715f0f6f160727dae2186c08b195dcde7e3
kouslab <labhmong123@gmail.com>
2025-04-20 23:40:07 -0500

    Update DBConnection.java

--- airline_scheduling/src/main/java/com/airline/DBConnection.java
+++ airline_scheduling/src/main/java/com/airline/DBConnection.java
@@ -15,8 +15,8 @@ import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/airline_scheduling";
-    private static final String USER = "root"; // Change if needed
-    private static final String PASSWORD = ""; // Change if needed
+    private static final String USER = ""; 
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
